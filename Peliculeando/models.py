db44fb7504e22edc4257e71dc8cf4d59105fd681
Bustos Ivan <ivanbustos2004@gmail.com>
2023-04-20 22:56:13 -0300

    imagenes

--- Peliculeando/models.py
+++ Peliculeando/models.py
@@ -7,16 +7,13 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     nombre_pelicula = models.CharField(max_length=40)
-
     año_estreno = models.IntegerField(validators=[
         MinValueValidator(1895),
         MaxValueValidator(timezone.now().year)])
-
     reseña_pelicula = models.CharField(max_length=800)
-
     valoracion_final = models.CharField(max_length=50) 
-
     autorizado = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="autorizado")
+    imagen = models.ImageField(upload_to="img", null=True, blank=True)
 
     def __str__(self):
         return f"{self.id} - {self.nombre_pelicula} ({self.año_estreno})"
--- proyecto_final/urls.py
+++ proyecto_final/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Peliculeando.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +32,5 @@ urlpatterns = [
     path('login/', Login.as_view(), name="login"),
     path('logout/', Logout.as_view(), name="logout"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
